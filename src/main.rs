0a0a634d29373f2633d90d0527a34a6ba331e59e
zombie110year <zombie110year@outlook.com>
2021-03-01 14:51:45 +0800

    功能（amiya-say）：读取命令行参数

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
 use amiya_say::build;
+use clap::{App, Arg, SubCommand};
 
 fn main() {
-    _test();
+    let app = App::new("amiya-say").subcommand(
+        SubCommand::with_name("say")
+            .arg(
+                Arg::with_name("role")
+                    .short("r")
+                    .long("role")
+                    .default_value("amiya.term"),
+            )
+            .arg(Arg::with_name("text")),
+    );
+    let args = app.get_matches();
+    match args.subcommand() {
+        ("say", Some(say_args)) => {
+            println!(
+                "{}",
+                build(
+                    say_args.value_of("text").unwrap(),
+                    say_args.value_of("role").unwrap()
+                )
+            )
+        }
+        _ => panic!(),
+    }
 }
 
 fn _test() {
