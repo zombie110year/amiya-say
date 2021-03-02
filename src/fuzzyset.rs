c8238efbc0363c72cb1b900e31832b5d59ff9c23
zombie110year <zombie110year@outlook.com>
2021-03-02 14:43:46 +0800

    功能（fuzzyset）：使用 LevenShtein 距离匹配相近字符串

--- src/fuzzyset.rs
+++ src/fuzzyset.rs
@@ -1,31 +1,20 @@
 //! # 模糊集匹配相近字符串
 
+use triple_accel::levenshtein_exp;
+
 /// 从 candidates 中匹配出最接近 target 的字符串
 pub fn fuzzymatch(target: String, candidates: Vec<String>) -> String {
-    let target_vec: Vec<f32> = target
-        .as_bytes()
-        .iter()
-        .map(|b| b.to_owned() as f32)
-        .collect();
-    let candidates_vec: Vec<Vec<f32>> = candidates
-        .iter()
-        .map(|s| s.as_bytes().iter().map(|b| b.to_owned() as f32).collect())
-        .collect();
+    let target_bytes = target.as_bytes();
+    let candidates_bytes: Vec<&[u8]> = candidates.iter().map(|b| b.as_bytes()).collect();
 
-    let mut min_distance = f32::MAX;
+    let mut min_distance = u32::MAX;
     let mut similar_string = String::new();
-    for (cand, cand_vec) in candidates.iter().zip(candidates_vec.iter()) {
-        let dist = distance_between_vec(cand_vec, &target_vec);
+    for (cand, cand_bytes) in candidates.iter().zip(candidates_bytes.iter()) {
+        let dist = levenshtein_exp(cand_bytes, &target_bytes);
         if dist < min_distance {
             min_distance = dist;
             similar_string = cand.to_owned();
         }
     }
     return similar_string;
-}
-
-fn distance_between_vec(v1: &Vec<f32>, v2: &Vec<f32>) -> f32 {
-    let delta: Vec<f32> = v1.iter().zip(v2.iter()).map(|(a, b)| a - b).collect();
-    let distance = delta.iter().map(|x| x.powf(2.0)).sum::<f32>().sqrt();
-    return distance;
 }
