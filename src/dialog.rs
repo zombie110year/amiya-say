aeb05b164821ba4d606a2647df76f3138fbbd01c
zombie110year <zombie110year@outlook.com>
2021-03-02 15:57:43 +0800

    格式（dialog）：优化了对话框排版代码的可读性

--- src/dialog.rs
+++ src/dialog.rs
@@ -17,36 +17,46 @@ pub fn dialog_nobox(text: String) -> Result<String, std::fmt::Error> {
     for c in text.chars() {
         length += if c.is_ascii() { 1 } else { 2 };
     }
-    let lines = length / DIALOG_WIDTH;
-    let lines = if DIALOG_WIDTH * lines >= length {
-        lines
-    } else {
-        lines + 1
-    };
-    let mut dialog_box =
-        String::with_capacity(DIALOG_WIDTH * lines + 2 * (DIALOG_WIDTH + lines) + 4);
 
-    let mut line_width = 0;
-    let mut chars = text.chars();
-    if let Some(c) = chars.next() {
-        let char_width = if c.is_ascii() { 1 } else { 2 };
-        dialog_box.write_char(c)?;
-        line_width += char_width;
-    }
-    for c in chars {
-        let char_width = if c.is_ascii() { 1 } else { 2 };
-        if line_width + char_width <= DIALOG_WIDTH {
-            dialog_box.write_char(c)?;
-            line_width += char_width;
-        } else {
-            // 行末
-            dialog_box.write_str("\n")?;
-            dialog_box.write_char(c)?;
-            line_width = char_width;
+    let last_line_remain = length % DIALOG_WIDTH;
+    let lines = if last_line_remain == 0 {
+        length / DIALOG_WIDTH
+    } else {
+        length / DIALOG_WIDTH + 1
+    };
+    let mut dialog_box = String::with_capacity(
+        length - last_line_remain + DIALOG_WIDTH + 2 * (DIALOG_WIDTH + lines) + 4,
+    );
+
+    let mut chars = text.chars().peekable();
+    'outer: loop {
+        let mut line_width = 0;
+        'inner: loop {
+            if let Some(ch) = chars.peek() {
+                let char_width = if ch.is_ascii() { 1 } else { 2 };
+                if line_width + char_width <= DIALOG_WIDTH {
+                    // 消耗迭代器
+                    let ch = chars.next().unwrap();
+                    dialog_box.write_char(ch)?;
+                    line_width += char_width;
+                } else {
+                    // 一行的末尾
+                    break 'inner;
+                }
+            } else {
+                // 最后一行
+                let whitespaces = DIALOG_WIDTH - line_width;
+                if whitespaces > 0 {
+                    dialog_box.write_str(" ".repeat(whitespaces).as_str())?;
+                }
+                dialog_box.write_str("\n")?;
+                break 'outer;
+            }
         }
+        dialog_box.write_str("\n")?;
     }
-    // 最后一行
-    dialog_box.write_str("\n")?;
+
+    // 框线
     dialog_box.write_str("   \\")?;
     dialog_box.write_str("\n")?;
 
@@ -58,54 +68,78 @@ pub fn dialog_withbox(text: String) -> Result<String, std::fmt::Error> {
     for c in text.chars() {
         length += if c.is_ascii() { 1 } else { 2 };
     }
-    let lines = length / DIALOG_WIDTH;
-    let lines = if DIALOG_WIDTH * lines >= length {
-        lines
-    } else {
-        lines + 1
-    };
-    let mut dialog_box =
-        String::with_capacity(DIALOG_WIDTH * lines + 2 * (DIALOG_WIDTH + lines) + 4);
-
-    dialog_box.write_str(&TABLE_LU)?;
-    dialog_box.write_str(TABLE_HR.repeat(DIALOG_WIDTH).as_str())?;
-    dialog_box.write_str(&TABLE_RU)?;
-    dialog_box.write_str("\n")?;
-
-    let mut line_width = 0;
-    let mut chars = text.chars();
-    if let Some(c) = chars.next() {
-        let char_width = if c.is_ascii() { 1 } else { 2 };
+    if length <= DIALOG_WIDTH {
+        let mut dialog_box = String::with_capacity(length + 2 * length + 6);
+        dialog_box.write_str(&TABLE_LU)?;
+        dialog_box.write_str(TABLE_HR.repeat(length).as_str())?;
+        dialog_box.write_str(&TABLE_RU)?;
+        dialog_box.write_str("\n")?;
         dialog_box.write_str(&TABLE_VT)?;
-        dialog_box.write_char(c)?;
-        line_width += char_width;
-    }
-    for c in chars {
-        let char_width = if c.is_ascii() { 1 } else { 2 };
-        if line_width + char_width <= DIALOG_WIDTH {
-            dialog_box.write_char(c)?;
-            line_width += char_width;
+        dialog_box.write_str(text.as_str())?;
+        dialog_box.write_str(&TABLE_VT)?;
+        dialog_box.write_str("\n")?;
+        dialog_box.write_str(&TABLE_LD)?;
+        dialog_box.write_str(&TABLE_AG)?;
+        dialog_box.write_str(TABLE_HR.repeat(length - 1).as_str())?;
+        dialog_box.write_str(&TABLE_RD)?;
+        dialog_box.write_str("\n")?;
+        return Ok(dialog_box);
+    } else {
+        let last_line_remain = length % DIALOG_WIDTH;
+        let lines = if last_line_remain == 0 {
+            length / DIALOG_WIDTH
         } else {
-            // 行末
+            length / DIALOG_WIDTH + 1
+        };
+        let mut dialog_box = String::with_capacity(
+            length - last_line_remain + DIALOG_WIDTH + 2 * (DIALOG_WIDTH + lines) + 4,
+        );
+
+        dialog_box.write_str(&TABLE_LU)?;
+        dialog_box.write_str(TABLE_HR.repeat(DIALOG_WIDTH).as_str())?;
+        dialog_box.write_str(&TABLE_RU)?;
+        dialog_box.write_str("\n")?;
+
+        let mut chars = text.chars().peekable();
+        'outer: loop {
+            let mut line_width = 0;
+            dialog_box.write_str(&TABLE_VT)?;
+            'inner: loop {
+                if let Some(ch) = chars.peek() {
+                    let char_width = if ch.is_ascii() { 1 } else { 2 };
+                    if line_width + char_width <= DIALOG_WIDTH {
+                        // 消耗迭代器
+                        let ch = chars.next().unwrap();
+                        dialog_box.write_char(ch)?;
+                        line_width += char_width;
+                    } else {
+                        // 一行的末尾
+                        let whitespaces = DIALOG_WIDTH - line_width;
+                        if whitespaces > 0 {
+                            dialog_box.write_str(" ".repeat(whitespaces).as_str())?;
+                        }
+                        break 'inner;
+                    }
+                } else {
+                    // 最后一行
+                    let whitespaces = DIALOG_WIDTH - line_width;
+                    if whitespaces > 0 {
+                        dialog_box.write_str(" ".repeat(whitespaces).as_str())?;
+                    }
+                    dialog_box.write_str(&TABLE_VT)?;
+                    dialog_box.write_str("\n")?;
+                    break 'outer;
+                }
+            }
             dialog_box.write_str(&TABLE_VT)?;
             dialog_box.write_str("\n")?;
-            // 第二行行首
-            dialog_box.write_str(&TABLE_VT)?;
-            dialog_box.write_char(c)?;
-            line_width = char_width;
         }
+        dialog_box.write_str(&TABLE_LD)?;
+        dialog_box.write_str(&TABLE_AG)?;
+        dialog_box.write_str(TABLE_HR.repeat(DIALOG_WIDTH - 1).as_str())?;
+        dialog_box.write_str(&TABLE_RD)?;
+        dialog_box.write_str("\n")?;
+
+        return Ok(dialog_box);
     }
-    // 最后一行
-    let remain_width = DIALOG_WIDTH - line_width;
-    dialog_box.write_str(" ".repeat(remain_width).as_str())?;
-    dialog_box.write_str(&TABLE_VT)?;
-    dialog_box.write_str("\n")?;
-
-    dialog_box.write_str(&TABLE_LD)?;
-    dialog_box.write_str(&TABLE_AG)?;
-    dialog_box.write_str(TABLE_HR.repeat(DIALOG_WIDTH - 1).as_str())?;
-    dialog_box.write_str(&TABLE_RD)?;
-    dialog_box.write_str("\n")?;
-
-    return Ok(dialog_box);
 }
